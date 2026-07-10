889d0960ddbbb1eccdb6d676e768051a1c9ecfb0
Tran Dinh Si <trandinhsi1401@gmail.com>
2026-07-10 22:50:36 +0700

    Sửa một số cấu hình

--- apps/backend/src/main/java/com/iuh/pharmacy_project/entity/ProductType.java
+++
@@ -1,5 +0,0 @@
-package com.iuh.pharmacy_project.entity;
-
-public enum ProductType {
-    THUOC_KE_DON, THUOC_KHONG_KE_DON, THUC_PHAM_CHUC_NANG;
-}
--- apps/backend/src/main/java/com/iuh/pharmacy_project/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iuh.pharmacy_project.repository;
-
-import com.iuh.pharmacy_project.entity.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
